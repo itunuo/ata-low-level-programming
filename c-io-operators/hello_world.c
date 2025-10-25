f29897b4370ccd1585ef3effcef715c4c736b4ad
Sururat <itunuoluwa2106@gmail.com>
2025-10-25 13:28:26 +0100

    new directory

---
+++ c-io-operators/hello_world.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello World!\n"); 
+    printf(" Insert your name:"); 
+    char name[10]; 
+    scanf("%s", name); 
+    printf("Your name is %s", name); 
+}
