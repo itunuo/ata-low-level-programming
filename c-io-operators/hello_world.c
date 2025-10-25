e5989e3cfc2ce7d7d16f9f20018c16a800013ef2
Sururat <itunuoluwa2106@gmail.com>
2025-10-25 14:17:07 +0100

    new

--- c-io-operators/hello_world.c
+++ c-io-operators/hello_world.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello World!\n"); 
-    printf(" Insert your name:"); 
-    char name[10]; 
-    scanf("%s", name); 
-    printf("Your name is %s", name); 
+    // printf("Hello World!\n"); 
+    // printf(" Insert your name:"); 
+    // char name[10]; 
+    // scanf("%s", name); 
+    // printf("Your name is %s\n", name); 
+
+    // a progam that adds two integers
+    int x,y, Result;
+    printf("first number: ");
+    scanf("%d\n", &x);
+    printf("Second Number");
+    scanf("%d", &y);
+    Result = x + y ;
+    printf("The sum of the numbers is : %d", Result );
 }
--- hello.c
+++ hello.c
@@ -3,4 +3,14 @@
 int main()
 {
     printf("Hello world !");
+    char str[5];
+      
+    // Read string
+    // from the user
+    scanf("%s",str);
+      
+    // Print the string
+    printf("%s",str);
+    return 0;
+
 }
